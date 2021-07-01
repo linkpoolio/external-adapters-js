4c0e8fe88fee7a35bbb460ad29faecd749b9e429
Grant Steuart <gmsteuart@gmail.com>
2021-07-01 09:28:06 -0500

    fix: fix stats-perform typing

--- packages/sources/stats-perform/src/config.ts
+++ packages/sources/stats-perform/src/config.ts
@@ -1,11 +1,15 @@
 import { Requester, util } from '@chainlink/ea-bootstrap'
-import { Config } from '@chainlink/types'
+import { Config as ChainlinkConfig } from '@chainlink/types'
 
 export const NAME = 'STATS_PERFORM'
 export const DEFAULT_BASE_URL = 'http://api.stats.com/'
 
+export type Config = ChainlinkConfig & {
+  secret?: string
+}
+
 export const makeConfig = (prefix?: string): Config => {
-  const config = Requester.getDefaultConfig(prefix)
+  const config: Config = Requester.getDefaultConfig(prefix)
   config.api.baseURL = config.api.baseURL || DEFAULT_BASE_URL
   config.apiKey = util.getEnv('API_KEY', prefix)
   config.secret = util.getEnv('SECRET', prefix)
