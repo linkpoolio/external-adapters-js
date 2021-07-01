2cfe5bdb427a749e1bbd1bdd4bf0d161c4cbd343
Grant Steuart <gmsteuart@gmail.com>
2021-07-01 14:51:30 -0500

    fix: stats-perform typings

--- packages/sources/stats-perform/src/endpoint/medals.ts
+++ packages/sources/stats-perform/src/endpoint/medals.ts
@@ -1,7 +1,29 @@
 import { Requester, Validator } from '@chainlink/ea-bootstrap'
-import { Config, ExecuteWithConfig } from '@chainlink/types'
+import { ExecuteWithConfig } from '@chainlink/types'
 import { createHash } from 'crypto'
+import { Config } from '../config'
 
+
+interface MedalParams {
+  sig?: string
+  season?: number | string
+  api_key: string
+  sortType?: string
+}
+
+interface MedalStat {
+  olympicCountry: {
+    countryId: number
+    name: string
+    abbreviation: string
+  }
+  medalCount: {
+    gold: number
+    silver: number
+    bronze: number
+    total: number
+  }
+}
 export const NAME = 'medals'
 
 const customError = (data: any) => data.Response === 'Error'
@@ -16,19 +38,28 @@ export const execute: ExecuteWithConfig<Config> = async (request, config) => {
   if (validator.error) throw validator.error
 
   const { apiKey, secret } = config
+
+  if (!apiKey) {
+    throw new Error(`API_KEY env var not set`)
+  }
+
   const timeFromEpoch = Math.floor(new Date().getTime() / 1000)
   const jobRunID = validator.validated.id
   const season = validator.validated.data.season
+  const url = `/v1/stats/oly/smr_oly/medals/`
   // Default sort is by overall medals, otherwise `gold` is an option
   let sortType = validator.validated.data.sortType
 
-  const url = `/v1/stats/oly/smr_oly/medals/`
-  const sig = createHash('sha256').update(apiKey + secret + timeFromEpoch).digest('hex')
-
-  let params = {
-    sig,
-    season,
+  let params: MedalParams = {
     api_key: apiKey
+  }
+
+  if (apiKey && secret && timeFromEpoch) {
+    params.sig = createHash('sha256').update(apiKey + secret + timeFromEpoch).digest('hex')
+  }
+
+  if (season) {
+    params.season = season
   }
 
   if (sortType) {
@@ -48,7 +79,7 @@ export const execute: ExecuteWithConfig<Config> = async (request, config) => {
   const { apiResults } = response.data
   const [olympics] = apiResults
   const { medals } = olympics.league
-  const abbrs = medals.map(m => m.olympicCountry.abbreviation)
+  const abbrs = medals.map((m: MedalStat) => m.olympicCountry.abbreviation)
 
   response.data.result = [abbrs[0], abbrs[1], abbrs[2]]
 
